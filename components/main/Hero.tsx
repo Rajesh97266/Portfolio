ba78e28637025c4040df2916acc63c0f18f04c2f
Rajesh97266 <rajesh97266@gmail.com>
2025-02-15 23:06:41 +0530

    global-css-added

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -8,7 +8,9 @@ const Hero = () => {
         muted
         loop
         className="rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover "
-      ></video>
+      >
+        <source src="/blackhole.webm" type="video/webm" />
+      </video>
     </div>
   );
 }

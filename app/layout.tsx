4e4551c74ebb03a6f26d8c150446f3432b5447c5
Rajesh97266 <rajesh97266@gmail.com>
2025-02-15 23:13:47 +0530

    home-page-completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from "@/components/main/Navbar";
+import Footer from "@/components/main/Footer";
+import StarsCanvas from "@/components/main/StarBackground";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Rajesh Portfolio',
-  description: 'This is mt Portfoilio',
+  description: 'This is mt Portfolio',
 }
 
 export default function RootLayout({
@@ -14,9 +17,16 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>{children}</body>
-    </html>
-  )
+ return (
+   <html lang="en">
+     <body
+       className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+     >
+       <StarsCanvas />
+       <Navbar />
+       {children}
+       <Footer />
+     </body>
+   </html>
+ );
 }

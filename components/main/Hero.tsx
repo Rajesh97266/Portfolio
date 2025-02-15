f816455d005e31fcf2585f3a58157fc0f8ff685a
Rajesh97266 <rajesh97266@gmail.com>
2025-02-15 19:55:36 +0530

    source-materials-added

---
+++ components/main/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="relative flex flex-col h-full w-full">
+      <video
+        autoPlay
+        muted
+        loop
+        className="rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover "
+      ></video>
+    </div>
+  );
+}
+
+export default Hero
